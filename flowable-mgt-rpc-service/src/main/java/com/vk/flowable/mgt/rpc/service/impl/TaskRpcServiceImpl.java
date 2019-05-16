132cc597f7bbdc339daaf3f2ce0546830b1d9e8e
zhaoml529 <zhaoml529@163.com>
2019-05-16 18:26:36 +0800

    init project

--- flowable-mgt-rpc-service/src/main/java/com/vk/flowable/mgt/rpc/service/impl/TaskRpcServiceImpl.java
+++ flowable-mgt-rpc-service/src/main/java/com/vk/flowable/mgt/rpc/service/impl/TaskRpcServiceImpl.java
@@ -1,7 +1,9 @@
-package com.vk.flowable.mgt.rpc.service;
+package com.vk.flowable.mgt.rpc.service.impl;
+
+import com.vk.flowable.mgt.rpc.api.service.TaskRpcService;
 
 /**
  * Created by zml on 2019/5/16.
  */
-public class TaskRpcServiceImpl {
+public class TaskRpcServiceImpl implements TaskRpcService {
 }
--- flowable-mgt-service/src/main/java/com/vk/flowable/mgt/service/impl/TaskServiceImpl.java
+++ flowable-mgt-service/src/main/java/com/vk/flowable/mgt/service/impl/TaskServiceImpl.java
@@ -1,7 +1,9 @@
 package com.vk.flowable.mgt.service.impl;
 
+import com.vk.flowable.mgt.service.TaskService;
+
 /**
  * Created by zml on 2019/5/16.
  */
-public class TaskServiceImpl {
+public class TaskServiceImpl implements TaskService {
 }
