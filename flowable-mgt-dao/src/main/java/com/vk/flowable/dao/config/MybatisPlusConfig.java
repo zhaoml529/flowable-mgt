175c582f423fca7db95e321856550e0cc9c0a2b9
zhaoml529 <zhaoml529@163.com>
2019-05-28 18:13:56 +0800

    update .gitignore

---
+++ flowable-mgt-dao/src/main/java/com/vk/flowable/dao/config/MybatisPlusConfig.java
@@ -0,0 +1,43 @@
+package com.vk.flowable.dao.config;
+
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@MapperScan("com.vk.flowable.dao.mapper")
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+
+        return new PaginationInterceptor();
+    }
+
+    /**
+     * 开启乐观锁更新 配合@Version注解使用
+     * @return
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor () {
+
+        return new OptimisticLockerInterceptor();
+    }
+
+    /**
+     * 逻辑删除 配合@TableLogic注解使用
+     * @return
+     */
+    @Bean
+    public ISqlInjector sqlInjector() {
+        return new LogicSqlInjector();
+    }
+}
---
+++ flowable-mgt-dao/src/test/java/com/vk/flowable/GeneratorServiceEntity.java
@@ -0,0 +1,56 @@
+package com.vk.flowable;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import org.junit.Test;
+
+public class GeneratorServiceEntity {
+
+    @Test
+    public void generateCode() {
+        String packageName = "com.vk.flowable";
+        generateByTables(packageName, "message_send_task");
+    }
+
+    private void generateByTables(String packageName, String... tableNames) {
+        GlobalConfig config = new GlobalConfig();
+        String dbUrl = "jdbc:mysql://localhost:3306/flowable";
+        DataSourceConfig dataSourceConfig = new DataSourceConfig();
+        dataSourceConfig.setDbType(DbType.MYSQL)
+                .setUrl(dbUrl)
+                .setUsername("root")
+                .setPassword("")
+                .setDriverName("com.mysql.jdbc.Driver");
+        StrategyConfig strategyConfig = new StrategyConfig();
+        strategyConfig
+                .setCapitalMode(false)
+                .setEntityLombokModel(true)
+                .setNaming(NamingStrategy.underline_to_camel)
+                .setTablePrefix("tb_")
+                .setVersionFieldName("version")
+                .setLogicDeleteFieldName("deleted")
+                .setInclude(tableNames);//修改替换成你需要的表名，多个表名传数组
+        config.setActiveRecord(false)
+                .setAuthor("zml")
+                .setOutputDir("/Users/zml/Desktop/GeneratorCode")
+                .setEnableCache(false)
+                .setBaseResultMap(true)
+                .setBaseColumnList(true)
+                .setFileOverride(true)
+                .setServiceName("%sService");
+        new AutoGenerator().setGlobalConfig(config)
+                .setDataSource(dataSourceConfig)
+                .setStrategy(strategyConfig)
+                .setPackageInfo(
+                        new PackageConfig()
+                                .setParent(packageName)
+                                .setController("controller")
+                                .setEntity("domain")
+                ).execute();
+    }
+}
